a80aaed3c3c5e5669ba8b92a278f5246965f54cb
Guangchuang Yu <guangchuangyu@gmail.com>
2019-06-17 23:36:14 +0800

    fixed bug to work with case-sensitive file system, e.g. to successfully compile in Linux

--- R/AbLandscapes.R
+++ R/AbLandscapes.R
@@ -10,6 +10,6 @@
 #' @importFrom graphics abline axis box boxplot hist layout lines
 #' @importFrom graphics mtext par plot.new plot.window points polygon text title
 #' @importFrom stats dist lm median nlminb optim pchisq pnorm prcomp t.test
-#' @useDynLib AbLandscapes
+#' @useDynLib ablandscapes
 #'
 NULL
