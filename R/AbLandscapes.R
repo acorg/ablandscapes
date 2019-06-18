89ddabc656096b0103063bab89c2b9aee8c880b3
Sam Wilks <sw463@cam.ac.uk>
2019-06-18 09:54:43 +0100

    Bug fix

--- R/AbLandscapes.R
+++ R/AbLandscapes.R
@@ -1,15 +1,15 @@
-#' AbLandscapes: Making Antibody-landscapes Using R
+#' ablandscapes: Making Antibody-landscapes Using R
 #'
 #' A package containing functions for making antibody landscapes.
 #'
 #' @docType package
-#' @name AbLandscapes
+#' @name ablandscapes
 #'
 #' @importFrom Rcpp evalCpp
 #' @importFrom grDevices chull col2rgb rgb
 #' @importFrom graphics abline axis box boxplot hist layout lines
 #' @importFrom graphics mtext par plot.new plot.window points polygon text title
 #' @importFrom stats dist lm median nlminb optim pchisq pnorm prcomp t.test
-#' @useDynLib AbLandscapes
+#' @useDynLib ablandscapes
 #'
 NULL
